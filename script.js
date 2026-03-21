ea928498e6c92c1317311702ec18c5f7441bc642
kulkarnidhruva06-bit <kulkarnidhruva06@gmail.com>
2026-03-20 20:53:17 -0400

    A meaningful commit message

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-fetch("https://api.countapi.xyz/hit/creativityforall.fun/visits")
+fetch("https://api.countagpi.xyz/hit/creativityforall.fun/kulkarnidhruva06-bit's Workspace")
   .then(function(res){ return res.json(); })
   .then(function(data){
     document.getElementById("visitorCount").textContent =
